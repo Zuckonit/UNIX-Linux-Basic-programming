92d6be020a7226b6a85d23d6b8b4816032872d04
Yucoat <thlgood@yucoat.com>
2011-12-06 17:56:17 +0800

    Classify source code!

---
+++ InputOutput/output.c
@@ -0,0 +1,44 @@
+/*
+ *title:		UNIX/Linux print series	
+ *
+ *author:	yucoat thlgood(AT)yucoat.com www.yucoat.com
+ *
+ *date:		6 Dec 2011
+ *
+ *version:	0.1
+ *
+ *log:		NULL
+ */
+#include "thlgood.h"
+
+int main(void)
+{
+	char Str[20];
+	char p[20];
+
+	FILE *fp;
+	if ((fp = fopen("out", "wb")) == NULL)
+	{
+		perror("fopen");
+		exit(-1);
+	}
+
+	/* gets()function is very dangerous! */
+	gets(Str);
+	Ps(Str);		//Ps() is print string!
+
+	/* fgets()function is more safe than gets() */
+	fgets(p, 19, stdin);
+	printf("P=\n");
+	Ps(p);
+
+	sprintf(Str, "%s", p);
+	Ps(Str);
+	snprintf(Str, strlen(p) - 1, "%s", p);
+	Ps(Str);
+
+	fprintf(fp, "%s", Str);
+
+	fclose(fp);
+	return 0;
+}
