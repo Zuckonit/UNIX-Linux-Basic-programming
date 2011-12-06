d3af912aa82406c1cb5583ff0e43a297b71c46d6
Yucoat <thlgood@yucoat.com>
2011-12-06 13:12:44 +0800

    adjust format open.c

--- open.c
+++ open.c
@@ -13,27 +13,26 @@
 #include <stdio.h>		// for IO
 #include <fcntl.h>		//for open()
 #include <stdlib.h>		//for exit()
+#include "thlgood.h"
 
 int main(int argc, char *argv[])
 {
 	int count;
-	int fd;				//file descript
+	int fd;			//file descript
 	if (argc < 2)
 	{
 		printf("Arguments is too less !\n");
 		exit(0);
 	}
 
-	for (count = 1; count < argc; count++)
+	if ((fd = open(argv[count], O_RDONLY)) == -1)
+		printf("No!, file not exist\n");
+	else
 	{
-		if ((fd = open(argv[count], O_RDONLY)) == -1)
-			printf("No!, file not exist\n");
-		else
-		{
-			printf("Yes, file exist!\n", fd);
-			close(fd);
-		}
+		printf("Yes, file exist!\n", fd);
+		close(fd);
 	}
+
 	return 0;
 }
 
