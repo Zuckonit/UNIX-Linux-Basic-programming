650ac7fac3b8aca902a4e25f91b91d1a49186f4b
Yucoat <thlgood@yucoat.com>
2011-12-06 12:36:31 +0800

    Add my header file

---
+++ thlgood.h
@@ -0,0 +1,33 @@
+#ifndef _THLGOOD_H_
+#define _THLGOOD_H_
+
+/*	For new Line */
+#define _NEWLINE_ 		putchar("n\")
+
+/* print "Hello, World" */
+#define ECHO_MSG		printf("Hello, World\n")
+
+/* Get Max Value */
+#define SWAP(type)		void swap##type(type *a, type *b)	\
+						{									\
+							*a = *a + *b;					\
+							*b = *a - *b;					\
+							*a = *a - *b;					\
+						}
+
+/* print a string */
+void define Ps(a)
+{
+	printf("%s\n", a);
+}
+
+/* print integer */
+void Pi(a)
+{
+	printf("%d\n", a)
+}
+
+/* Get max value */
+#define MAX(type)		type max##type()
+
+#endif
